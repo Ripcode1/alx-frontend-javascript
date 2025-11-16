4f028db9d1c88e2dcb50f7d3fe3251ad4f2e664d
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-11-16 23:55:18 +0200

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -39,8 +39,10 @@ class Teacher implements TeacherInterface {
 }
 
 function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === "number" && salary < 500) {
-    return new Teacher();
+  if (typeof salary === 'number') {
+    if (salary < 500) {
+      return new Teacher();
+    }
   }
   return new Director();
 }
@@ -59,7 +61,7 @@ function executeWork(employee: Director | Teacher): string {
 
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
   if (todayClass === "Math") {
     return "Teaching Math";
   } else {
